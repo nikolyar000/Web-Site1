2dd236b4dcdab261f9854b0893ff9e6b3a4477a7
nikolyar000 <nikolyar000@battlegroundps.org>
2024-05-01 05:44:32 +0200

    Main
    Main

---
+++ index.js
@@ -0,0 +1,15 @@
+function openModal() {
+    document.getElementById('myModal').style.display = 'block';
+}
+
+function closeModal() {
+    document. getElementById('myModal').style.display = 'none';
+}
+
+function openModal1(){
+    document.getElementById('myModal1').style.display = 'block';
+}
+
+function closeModal1(){
+    document.getElementById('myModal1').style.display = 'none';
+}
